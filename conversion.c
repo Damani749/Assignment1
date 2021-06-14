fad919b6744ead4b05b173031f4d335d723afaeb
Damani749 <damani.ramya309@gmail.com>
2021-06-14 12:29:15 +0530

    secondconversion.c

---
+++ conversion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number: ");
+scanf("%d",&a);
+printf("Decimal value is: %d\n",a);
+printf("Octal value is: %o\n",a);
+printf("Hexadecial value is: %x\n",a);
+printf("character value is: %c\n",a);
+return 0;
+}
